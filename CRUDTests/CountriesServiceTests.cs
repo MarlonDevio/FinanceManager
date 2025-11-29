88e84e489bfe488cb0a0ebbd4966ba766b306a09
marlondevio <mar.hamaekers@gmail.com>
2025-11-29 20:39:22 +0100

    add unit test

---
+++ CRUDTests/CountriesServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using ServiceContracts;
+using ServiceContracts.DTO;
+
+namespace CRUDTests;
+
+public class CountriesServiceTests
+{
+    private ICountriesService _countriesService;
+    private CountryAddRequest _countryAddRequest;
+
+    [SetUp]
+    public void Setup()
+    {
+        _countriesService = new CountriesService();
+        _countryAddRequest = new CountryAddRequest
+        {
+            CountryName = "Spain"
+        };
+    }
+
+    [Test]
+    public void Test1()
+    {
+
+        var result = _countriesService.AddCountry(_countryAddRequest);
+        Assert.That(result, Is.Not.Null);
+    }
+}
+
+public class CountriesService : ICountriesService
+{
+    public CountryResponse AddCountry(CountryAddRequest? countryAddRequest)
+    {
+        return new CountryResponse();
+    }
+}
--- ServiceContracts/ICountriesService.cs
+++ ServiceContracts/ICountriesService.cs
@@ -1,6 +1,16 @@
-﻿namespace ServiceContracts;
+﻿using ServiceContracts.DTO;
 
+namespace ServiceContracts;
+
+/// <summary>
+/// Represents business logic for manipulating Country entity
+/// </summary>
 public interface ICountriesService
 {
-    
+  /// <summary>
+  /// Adds a country object to countries list
+  /// </summary>
+  /// <param name="countryAddRequest"><see cref="CountryAddRequest"/></param>
+  /// <returns><see cref="CountryResponse"/></returns>
+  CountryResponse AddCountry(CountryAddRequest? countryAddRequest);
 }
